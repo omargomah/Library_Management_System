84c80086c1fb46b125b418c96c4526d0466d9525
Omar Gomaa <mrjmh934@gmail.com>
2026-09-02 20:30:04 +0300

    fix type of name to string

--- LibraryManagementSystem/LibraryManagementSystem/Entities/Category.cs
+++ LibraryManagementSystem/LibraryManagementSystem/Entities/Category.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Reflection.Metadata.Ecma335;
-using System.Text;
-
-namespace LibraryManagementSystem.Entities
+﻿namespace LibraryManagementSystem.Entities
 {
     public class Category: Entity
     {
-        public int Name { get; set; }
+        public string Name { get; set; }
         public ICollection<Book> Books { get; set; } = new HashSet<Book>();
     }
 }
