9d4e0d76da62721415b607534f60b480c3a82376
Omar Gomaa <mrjmh934@gmail.com>
2026-09-02 23:16:06 +0300

    delete category is finish withou test

--- LibraryManagementSystem/LibraryManagementSystem/Managements/CategoryManagement.cs
+++ LibraryManagementSystem/LibraryManagementSystem/Managements/CategoryManagement.cs
@@ -25,13 +25,7 @@ namespace LibraryManagementSystem.Managements
             Console.WriteLine("--- Add New Category ---");
             Category newCategory = Category.Create(await _categoryRepository.GetAllCategoriesNameAsync());
             await _categoryRepository.AddAsync(newCategory);
-            if(await _unitOfWork.SaveChangesAsync() > 0)
-                Console.WriteLine("\nCategory added success");
-            else
-                Console.WriteLine("\nCategory added fail");
-
-            Console.WriteLine("Press any key to continue...");
-            Console.ReadKey();
+            EndMessageOfAddAndUpdateAndDelete("add", await _unitOfWork.SaveChangesAsync() > 0);
         }
         private int GetValidId()
         {
@@ -41,19 +35,32 @@ namespace LibraryManagementSystem.Managements
                 Console.Write("Invalid value enter another one: ");
             return id;
         }
-        private async Task<Category?> GetEntityByIdAndCheckIsValidOrNot(Func<int, Task<Category>> funcToGetCategoryById)
+        private async Task<Category?> GetEntityByIdAndCheckIsValidOrNot(Func<int, Task<Category?>> funcToGetCategoryById)
         {
-            Category? book = await funcToGetCategoryById.Invoke(GetValidId());
-            if (book is null)
+            Category? category = await funcToGetCategoryById.Invoke(GetValidId());
+            if (category is null)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("You enter invalid book Id try again");
                 Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
                 return null;
             }
-            return book;
+            return category;
         }
 
+        private void EndMessageOfAddAndUpdateAndDelete(string action ,bool IsSuccess)
+        {
+            if (IsSuccess)
+                Console.WriteLine($"\nCategory {action} success");
+            else
+                Console.WriteLine($"\nCategory {action} fail");
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+
+        }
 
         public async Task UpdateCategoryAsync()
         {
@@ -62,20 +69,34 @@ namespace LibraryManagementSystem.Managements
 
             Category? categoryWillUpdate = await GetEntityByIdAndCheckIsValidOrNot(_categoryRepository.GetByIdAsync);
             if (categoryWillUpdate is null)
+                return;
+            categoryWillUpdate.UpdateName(await _categoryRepository.GetAllCategoriesNameAsync());
+            _categoryRepository.Update(categoryWillUpdate);
+            EndMessageOfAddAndUpdateAndDelete("update", await _unitOfWork.SaveChangesAsync() > 0);
+        }
+        public async Task DeleteCategoryAsync()
+        {
+            Console.Clear();
+            Console.WriteLine("--- Delete Category ---");
+            
+            var category = await GetEntityByIdAndCheckIsValidOrNot(_categoryRepository.GetByIdAsync);
+            if (category == null)
+                return;
+
+            Console.Write($"Are you sure you want to delete '{category.Name}'? (y/n): ");
+            ConsoleKey confirm = Console.ReadKey().Key;
+
+            if (confirm == ConsoleKey.Y)
             {
+                _categoryRepository.Delete(category);
+                EndMessageOfAddAndUpdateAndDelete("delete", await _unitOfWork.SaveChangesAsync() > 0);
+            }
+            else
+            {
+                Console.WriteLine("\nOperation cancelled");
                 Console.WriteLine("Press any key to continue...");
                 Console.ReadKey();
-                return;
             }
-
-            categoryWillUpdate.UpdateName(await _categoryRepository.GetAllCategoriesNameAsync());
-
-            _categoryRepository.Update(category);
-            // await _categoryRepository.SaveChangesAsync();
-
-            Console.WriteLine("\nSuccess: Category updated successfully!");
-            Console.WriteLine("Press any key to continue...");
-            Console.ReadKey();
         }
     }
 }
