561ef8e3bcc31fdb171d177aa7b2bbf8010f2128
Omar Gomaa <mrjmh934@gmail.com>
2026-09-02 20:29:26 +0300

    Add Book management with all fun  like Add Book and Delete Book and Get Book By Id and Get All Books and Search and Filtering and use the rich model not anomic

---
+++ LibraryManagementSystem/LibraryManagementSystem/Dtos/CategoryDtos/SelectMenuOfCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManagementSystem.Dtos.CategoryDtos
+{
+    public record SelectMenuOfCategoryDto(int Id, string Name);
+}
