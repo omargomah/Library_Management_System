38bcb0957fcd44b6f83469d17e1bb68b5ffe713b
Omar Gomaa <mrjmh934@gmail.com>
2026-09-02 20:19:59 +0300

    Add All IRepo and Use the Generic repo to reduce the redundency

---
+++ LibraryManagementSystem/LibraryManagementSystem/Interfaces/IRepositories/IBookRepository.cs
@@ -0,0 +1,15 @@
+﻿using LibraryManagementSystem.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManagementSystem.Interfaces.IRepositories
+{
+    public interface IBookRepository: IRepository<Book>
+    {
+        Task<List<Book>> GetAllBooksWithCategoryAsync();
+        Task<Book?> GetBookWithCategoryByIdAsync(int bookId);
+        IQueryable<Book> GetAllBooksWithCategory();
+
+    }
+}
