4555788e24a6469bdd7400d5f2fb87192788e641
Omar Gomaa <mrjmh934@gmail.com>
2026-09-02 12:26:11 +0300

    add the configuration of all entities

---
+++ LibraryManagementSystem/LibraryManagementSystem/Data/Configuration/BookConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using LibraryManagementSystem.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManagementSystem.Data.Configuration
+{
+    public class BookConfiguration : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.Property(x => x.Title).HasMaxLength(Constants.MaxTitleLength);
+            builder.Property(x => x.Author).HasMaxLength(Constants.MaxNameLength);
+
+            builder.HasOne(x => x.Category)
+                .WithMany(x => x.Books)
+                .HasForeignKey(x => x.CategoryId);
+        }
+    }
+}
