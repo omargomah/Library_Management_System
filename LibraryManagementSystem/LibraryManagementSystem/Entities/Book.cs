4a6bb26750b6a403e2480df32e3adfd5fb9220d7
Omar Gomaa <mrjmh934@gmail.com>
2026-09-02 12:39:15 +0300

    update the datatype of Publishyear to  int not dateonly

--- LibraryManagementSystem/LibraryManagementSystem/Entities/Book.cs
+++ LibraryManagementSystem/LibraryManagementSystem/Entities/Book.cs
@@ -6,10 +6,10 @@ namespace LibraryManagementSystem.Entities
 {
     public class Book :Entity
     {
-        public string Titel { get; set; }
+        public string Title { get; set; }
         public string Author { get; set; }
         public double Price { get; set; }
-        public DateOnly PublishedYear { get; set; }
+        public int PublishedYear { get; set; }
         public int CategoryId { get; set; }
         public Category Category { get; set; }
         public ICollection<Borrowing> Borrowings { get; set; } = new HashSet<Borrowing>();
